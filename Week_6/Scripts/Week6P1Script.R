ab2153094cecde03f31de025c5e4a7791fc9ecde
bbonilla21 <brandon.bonilla.604@my.csun.edu>
2022-03-08 12:29:46 -0800

    Week 6

---
+++ Week_6/Scripts/Week6P1Script.R
@@ -0,0 +1,27 @@
+#########################################
+### R markdown 
+### Created by: Brandon Bonilla
+### Created on: 2022-03-01
+#######################################
+
+##### notes #####
+# 4 parts to markdown 
+# medadata: YAMAL - title & output (html)
+# 
+# 
+#
+# HEADER 1
+# Bold = **, italics = *
+# lines, lists, images
+# Code Chunks = CODE on top of R studio
+# = FALSE are for chunk options 
+# Global Options for = T
+
+
+
+#####################
+## Day 2 notes 
+## knitt html
+##50% 
+## hide code = ECHO
+
